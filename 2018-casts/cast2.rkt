7a46e858718d7d48599a0b23674b7a16dbc83211
Billy Troy Wooton <gcowootont@gmail.com>
2018-02-16 21:02:33 -1000

    Publish source for 2nd cast

--- 2018-casts/cast2.rkt
+++ 2018-casts/cast2.rkt
@@ -39,7 +39,72 @@
 (define (equal-lists? l1 l2 l3 l4)
   (and (equal? l1 l2) (equal? l1 l3) (equal? l1 l4)))
 
+; (procedures, lambdas, and conditionals)
 
+; method
+
+; Lambda calculus
+; a x b
+
+; (lambda (x) (+ x x))
+
+; ((lambda (x) (+ x x)) 2) => 4
+
+; (add-two 5 7)
+
+; Environment
+
+; {
+;  add-two: (lambda (x y) (+ x y))
+;  +: #<procedure>
+;  *: #<procedure>
+;  cons: #<procedure>
+; ....
+; }
+
+; (add-two 5 7)
+; ((lambda (x y) (+ x y)) 5 7)
+; (+ 5 7)
+; 12
+
+; Higher-order functions
+
+; map: (a -> b) -> a List -> b List
+; f(x) [x, x, x, x, ...] => [f(x), f(x), f(x), f(x), ...]
+
+; Closure
+
+(define make-adder (lambda (n)
+                     (lambda (x) (+ n x))))
+
+(define add10 (make-adder 10))
+
+
+; (define add10 (make-adder 10))
+; (define add10 ((lambda (n) (lambda (x) (+ n x))) 10))
+; (define add10 (lambda (x) (+ 10 x)))
+; {
+;  add10: (lambda (x) (+ 10 x))
+; }
+
+; (add10 5)
+; ((lambda (x) (+ 10 x)) 5)
+; (+ 10 5)
+; 15
+
+; {
+;  add10: (lambda (x) (+ n x))
+; }
+
+; Frame
+; child  parent
+; {   -> {
+;          make-adder: (lambda ...)
+;  n: 10   add10: (lambda (x) (+ x n))
+; }      }
+
+; (lambda (x) (+ x n))
+; pointer: -> child
 
 
 
